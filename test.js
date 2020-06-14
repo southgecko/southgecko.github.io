211988a788b1f9e56f66ac4963153feb43755156
southgecko <40776577+southgecko@users.noreply.github.com>
2020-06-14 16:40:59 -0400

    added js function to keep article elements at a certain hieght when side scrolling

--- test.js
+++ test.js
@@ -51,6 +51,15 @@ fetch(eCoinUrl)
     });
 })
 
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 100);
+    }
+}
+
+window.addEventListener("hashchange", offsetAnchor);
+
+window.setTimeout(offsetAnchor, 1);
 
 
 
