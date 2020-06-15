1b53f85f9111a3a3448b4f7f354a0ba1936490ab
southgecko <40776577+southgecko@users.noreply.github.com>
2020-06-15 02:36:59 -0400

    fixed scrolling so the page doesnt jump back to the top

--- test.js
+++ test.js
@@ -51,6 +51,7 @@ fetch(eCoinUrl)
     });
 })
 
+/*
 function offsetAnchor() {
     if(location.hash.length !== 0) {
         window.scrollTo(window.scrollX, window.scrollY - 100);
@@ -60,6 +61,6 @@ function offsetAnchor() {
 window.addEventListener("hashchange", offsetAnchor);
 
 window.setTimeout(offsetAnchor, 1);
-
+*/
 
 
